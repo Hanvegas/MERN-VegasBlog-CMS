955932140585ca5dda12d181c96cc30b7236aadb
Hanvegas <seanferdyan2@gmail.com>
2025-05-01 15:00:12 +0700

    Add Error Handler & middleware route not found

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const ExpressError = require('./utils/ExpressError')
 
 dotenv.config()
 const app = express()
@@ -20,8 +21,19 @@ mongoose.connect(process.env.MONGO_URI)
       .then(() => console.log("Connected to MongoDB"))
       .catch((err) => console.log(err))
 
+app.use('/', require('./routes/auth'))
+
 app.use('/blog', require('./routes/blogs'))
 
-app.use('/', require('./routes/auth'))
+app.use((req, res, next) => {
+      next(new ExpressError("Page Not Found", 404))
+})
+
+app.use((err, req, res, next) => {
+      const statusCode = err.statusCode || 500
+      res.status(statusCode).json({
+            message: err.message || 'Oh no, something went wrong',
+      })
+})
 
 app.listen(process.env.PORT)
