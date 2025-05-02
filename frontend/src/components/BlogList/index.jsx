98973a849de400d97058bd782aa60e22098cf359
Hanvegas <seanferdyan2@gmail.com>
2025-05-02 14:34:21 +0700

    Styling component Blog List

--- frontend/src/components/BlogList/index.jsx
+++ frontend/src/components/BlogList/index.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const BlogList = ({ children }) => {
       return (
             <div className='w-full max-w-screen-2xl mx-auto mt-9 px-8'>
-                  <h1 className='text-4xl font-semibold mb-2'>Blog</h1>
-                  <p className='font-medium text-lg text-black/50 mb-8'>Here, we share travel tips, destination guides, and stories that inpsire your next adventure</p>
+                  <div>
+                        <h1 className='text-4xl font-semibold mb-2'>Blog</h1>
+                        <p className='font-medium text-lg text-black/50 mb-8'>Explore the glamour and culture of the casino world — beyond the games.</p>
+                  </div>
                   <div className='grid grid-cols-3 gap-8'>
                         {children}
                   </div>
