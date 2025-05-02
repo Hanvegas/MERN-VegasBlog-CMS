c1eff13de435a7d4cf5d60fb6261f422e6ad9e84
Hanvegas <seanferdyan2@gmail.com>
2025-05-02 14:43:17 +0700

    Revisi-2 Styling component Blog List

--- frontend/src/components/BlogList/index.jsx
+++ frontend/src/components/BlogList/index.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 const BlogList = ({ children }) => {
       return (
             <div className='w-full max-w-screen-2xl mx-auto mt-9 px-8'>
-                  <div className='flex justify-between items-center'>
+                  <div className='flex justify-between items-start'>
                         <div>
                               <h1 className='text-4xl font-semibold mb-2'>Blog</h1>
                               <p className='font-medium text-lg text-black/50 mb-8'>Explore the glamour and culture of the casino world — beyond the games.</p>
                         </div>
-                        <Link to={"/create"} className='py-2 px-6 font-semibold text-white bg-black border-2 mr-4 rounded-md hover:bg-white hover:text-black hover:border-black transition-all duration-200'>Add New Blog</Link>
+                        <Link to={"/create"} className='py-2 px-6 font-semibold text-white bg-black border-2 mr-4 rounded-md hover:bg-white hover:text-black hover:border-black transition-all duration-200 mt-4'>Add New Blog</Link>
                   </div>
                   <div className='grid grid-cols-3 gap-8'>
                         {children}
