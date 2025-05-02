57de600666b618b9fefafab28d545ca04d3674e1
Hanvegas <seanferdyan2@gmail.com>
2025-05-02 23:01:10 +0700

    Revisi Footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       return (
             <div className='flex justify-center items-center w-full h-24 bg-black'>
                   <div className='flex items-end'>
-                        <h3 className='text-3xl font-semibold mr-2'>
+                        <h3 className='text-3xl font-bold mr-2'>
                               <span className='text-red-500'>Vegas</span>
                               <span className='text-white'>Blog</span>
                         </h3>
